84b5c1fae65ab296f7fde5f4974e800126c93fb7
nsernamee <ydccjs1937@163.com>
2025-10-25 15:00:01 +0800

    Update config.js
    hexo主题配置

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -2,10 +2,10 @@ const CONFIG = {
   HEXO_HOME_BANNER_ENABLE: true,
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
   HEXO_HOME_BANNER_GREETINGS: [
-    'Hi，我是一个程序员',
-    'Hi，我是一个打工人',
-    'Hi，我是一个干饭人',
-    '欢迎来到我的博客🎉'
+    'Hi，欢迎来到我的博客🎉',
+    'Hi，我是一名开发者',
+    'Hi，我是一名学生',
+    '祝你拥有美好的一天🎉'
   ], // 首页大图标语文字
 
   HEXO_HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
@@ -36,10 +36,10 @@ const CONFIG = {
   HEXO_ARTICLE_RECOMMEND: true, // 文章关联推荐
 
   HEXO_WIDGET_LATEST_POSTS: true, // 显示最新文章卡
-  HEXO_WIDGET_ANALYTICS: false, // 显示统计卡
+  HEXO_WIDGET_ANALYTICS: true, // 显示统计卡
   HEXO_WIDGET_TO_TOP: true,
   HEXO_WIDGET_TO_COMMENT: true, // 跳到评论区
-  HEXO_WIDGET_DARK_MODE: true, // 夜间模式
+  HEXO_WIDGET_DARK_MODE: false, // 夜间模式
   HEXO_WIDGET_TOC: true, // 移动端悬浮目录
 
   HEXO_THEME_COLOR: '#928CEE' // 主题色配置（默认为 #928CEE）
